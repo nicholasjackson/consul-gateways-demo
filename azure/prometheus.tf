743c4915d0aba139f1071216624f26508f7a4b5d
Nic Jackson <jackson.nic@gmail.com>
2019-07-02 10:16:33 +0100

    added prometheus and grafana

---
+++ azure/prometheus.tf
@@ -0,0 +1,15 @@
+resource "helm_release" "prometheus" {
+  depends_on = [kubernetes_cluster_role_binding.tiller]
+
+  name      = "prometheus"
+  chart     = "stable/prometheus"
+  namespace = "default"
+}
+
+resource "helm_release" "grafana" {
+  depends_on = [kubernetes_cluster_role_binding.tiller]
+
+  name      = "grafana"
+  chart     = "stable/grafana"
+  namespace = "default"
+}
