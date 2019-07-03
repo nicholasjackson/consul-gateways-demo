4d6ac2f110b21749de52c277b4a88f61e1b8893c
Nic Jackson <jackson.nic@gmail.com>
2019-07-03 12:52:06 +0100

    Updated to add stats

--- azure/consul.tf
+++ azure/consul.tf
@@ -54,6 +54,11 @@ resource "helm_release" "consul" {
 
   set {
     name  = "server.extraConfig"
-    value = "{'advertise_addr_wan': '${kubernetes_service.consul.load_balancer_ingress.0.ip}'}"
+    value = "{\"advertise_addr_wan\": \"${kubernetes_service.consul.load_balancer_ingress.0.ip}\"}"
+  }
+
+  set {
+    name  = "centralConfig.proxyDefaults"
+    value = "{\"envoy_dogstatsd_url\": \"udp://127.0.0.1:9125\"}"
   }
 }
