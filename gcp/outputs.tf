a3d9ed50df6920d26bc15a2b1a4d9bd47c87d924
Erik Veld <eveld@hashicorp.com>
2019-07-04 08:09:02 +0200

    This proabably does not work

--- gcp/outputs.tf
+++ gcp/outputs.tf
@@ -1,15 +1,3 @@
 output "loadbalancer_server" {
     value = google_compute_forwarding_rule.server.ip_address
 }
-
-output "loadbalancer_nomad" {
-    value = google_compute_forwarding_rule.nomad.ip_address
-}
-
-output "loadbalancer_consul" {
-    value = google_compute_forwarding_rule.consul.ip_address
-}
-
-output "loadbalancer_prometheus" {
-    value = google_compute_forwarding_rule.prometheus.ip_address
-}
